03cad57f413c7be3675731a07540794c53097f64
allie7019@naver.com <allie7019@naver.com>
2025-05-20 12:37:45 +0900

    20250520 JS review

---
+++ ch1/myapp/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <h1>hello, React!</h1>
+      <h2>첫 번째 component</h2>
+      <App2></App2>
+    </>
+  )
+}
+
+function App2(){
+  return (
+    <>
+      <h2>안녕하세요</h2>
+    </>
+  )
+}
+
+export default App
