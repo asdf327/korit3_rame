0f949e724d9b9ee835261674f6cbe52b2a7949d4
allie7019@naver.com <allie7019@naver.com>
2025-05-27 16:23:50 +0900

    React

--- ch5/routerapp/src/main.tsx
+++ ch5/routerapp/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-// import './index.css' !-> mul 설명으로 위해 주석
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
